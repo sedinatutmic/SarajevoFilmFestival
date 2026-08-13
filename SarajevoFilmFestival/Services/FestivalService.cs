d596d44bed971c9a8b3642a38f068378dcedadd6
Tutmic Sedina <sedina.tutmic.23@size.ba>
2026-08-13 15:12:07 +0200

    Add IAppService interface and initial FestivalService implementation

---
+++ SarajevoFilmFestival/Services/FestivalService.cs
@@ -0,0 +1,103 @@
+﻿using System.Runtime.InteropServices;
+using SarajevoFilmFestival.Repositories;
+using SarajevoFilmFestival.Models;
+
+namespace SarajevoFilmFestival.Services;
+
+public class FestivalService : IAppService
+{
+    private readonly Repository<Customer> _customerRepo;
+    private readonly Repository<Booking> _bookingRepo;
+    private readonly Repository<Screening> _screeningRepo;
+
+    public FestivalService(
+        Repository<Customer> customerRepo,
+        Repository<Booking> bookingRepo,
+        Repository<Screening> screeningRepo
+    )
+    {
+        customerRepo=_customerRepo;
+        bookingRepo=_bookingRepo;
+        screeningRepo=_screeningRepo;
+    }
+
+    public Screening AddScreening(string title, DateTime date, decimal ticketPrice, int totalCapacity,
+        int avaliableSeats)
+    {
+        if (totalCapacity <= 0 || totalCapacity > 500)
+        {
+            throw new ArgumentException("Total capacity must be greater than 0 and less than or equal to 500!")
+        }
+
+        if (ticketPrice <= 0)
+        {
+            throw new ArgumentException("Ticket price must be greater than 0!");
+        }
+
+        var newScreening = new Screening
+        {
+            Title = title,
+            Date = date,
+            TicketPrice = ticketPrice,
+            TotalCapacity = totalCapacity,
+            AvaliableSeats = avaliableSeats
+        };
+        _screeningRepo.Add(newScreening);
+        return new Screening();
+    }
+
+    public Customer addCustomer(string name, string lastName, string email, int phoneNumber)
+    {
+        if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(lastName))
+        {
+            throw new ArgumentException("First name and Last name can not be empty!");
+        }
+
+        var newCustomer = new Customer
+        {
+            Name = name,
+            LastName = lastName,
+            Email = email,
+            PhoneNumber = phoneNumber
+        };
+        
+        _customerRepo.Add(newCustomer);
+        return newCustomer;
+    }
+
+    public Booking assignBookingToCustomer(int customerId, int screeningId, int ticketCount)
+    {
+        var customer= _customerRepo.GetById(customerId);
+        var screening = _screeningRepo.GetById(screeningId);
+
+        if (customer == null || screening == null)
+        {
+            throw new InvalidOperationException("Customer or screening does not exist!");
+        }
+
+        if (ticketCount <= 0)
+        {
+            throw new ArgumentException("Ticket count must be greater than 0!");
+        }
+
+        if (screening.AvaliableSeats < ticketCount)
+        {
+            throw new InvalidOperationException("Capacity reached. Not enough avaliable seats!");
+        }
+
+        screening.AvaliableSeats -= ticketCount;
+        _screeningRepo.Update(screening);
+
+        var newBooking = new Booking
+        {
+            CustomerID = customerId,
+            ScreeningID = screeningId,
+            TicketCount = ticketCount,
+            TotalPrice = (int)(ticketCount * screening.TicketPrice),
+            CreatedAt = DateTime.Now,
+            Status = BookingStatus.Confirmed
+        };
+            _bookingRepo.Add(newBooking);
+            return newBooking;
+    }
+}
---
+++ SarajevoFilmFestival/Services/IAppService.cs
@@ -0,0 +1,21 @@
+﻿using SarajevoFilmFestival.Models;
+
+namespace SarajevoFilmFestival.Services;
+
+public interface IAppService
+{
+    Screening AddScreening(string title, int capacity, decimal ticketPrice, bool isActive);
+    void ActivateScreening(int screeningId);
+    void DeactivateScreening(int screeningId);
+
+    Customer addCustomer(string name, string lastName, string email, int phoneNumber);
+    Booking assignBookingToCustomer(int customerId, int screeningId, int ticketCount);
+
+    Dictionary<int, List<Booking>> GroupBookingByScreening();
+    Customer? GetCustomerWithMostTickets();
+    List<Booking> GetCancelledBookings();
+    
+    List<Customer>GetAllCustomers();
+    List<Screening>GetAllScreenings();
+    List<Booking>GetAllBookings();
+}
